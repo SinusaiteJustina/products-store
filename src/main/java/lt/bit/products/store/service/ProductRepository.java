569a9d7169add67243f6bfa509e6ad6d7a98058f
SinusaiteJustina <97383036+SinusaiteJustina@users.noreply.github.com>
2022-06-15 17:23:17 +0300

    ProductRepository

---
+++ src/main/java/lt/bit/products/store/service/ProductRepository.java
@@ -0,0 +1,11 @@
+package lt.bit.products.store.service;
+
+
+import lt.bit.products.store.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
